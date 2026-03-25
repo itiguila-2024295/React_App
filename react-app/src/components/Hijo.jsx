f03b8de4cd6416864222bf19fc39b8a068a9056a
itiguila-2024295 <itiguila-2024295@kinal.edu.gt>
2026-03-25 13:20:20 -0600

    add: se crea rama develop

--- react-app/src/components/Hijo.jsx
+++ react-app/src/components/Hijo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Hijo = ({mensaje}) => {
   return (
     <div>
-        <h3>Mensaje del padre: {mensaje}</h3>
+        <h3>Mensaje del padre : {mensaje}</h3>
     </div>
   )
 }
